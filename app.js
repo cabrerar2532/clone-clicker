680eafac0e8c1565773d712b866036df3b934f13
cabrerar <me_irl@outlook.com>
2023-01-02 20:35:28 -0600

    Clicking

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-console.log(`hello this is a test`);
+'use strict'
+const audio = new Audio('/assets/audio/click.mp3');
+let globalScore = 0;
+document.addEventListener('contextmenu', event => event.preventDefault());
+function domScoreLoader(score) {
+    document.querySelector('.globalScore').textContent = score;
+}
 
-document.write(`this is another test`)
+let domMuffinButton = document.querySelector('.theButton').addEventListener('click', function () {
+globalScore ++;
+domScoreLoader(globalScore);
+audio.play();
+})
