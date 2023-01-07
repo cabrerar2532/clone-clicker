e6b6013931d8346b4a4ce6649be7b264d0912bdc
cabrerar2532 <cabrerarocha881@gmail.com>
2023-01-06 21:33:32 -0600

    Added new upgrades

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-'use strict'
-const audio = new Audio('/assets/audio/click.mp3');
+"use strict";
+const audio = new Audio("/assets/audio/click.mp3");
 let globalScore = 0;
-document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+//functions
 function domScoreLoader(score) {
-    document.querySelector('.globalScore').textContent = score;
+  document.querySelector(".globalScore").textContent = score;
 }
-
-let domMuffinButton = document.querySelector('.theButton').addEventListener('click', function () {
-globalScore ++;
-domScoreLoader(globalScore);
-audio.play();
-})
+let upgradeScore = 0;
+//increment by one
+function incrementValue() {
+  while (upgradeScore) {
+    globalScore++;
+    domScoreLoader(globalScore);
+  }
+}
+// Click to increase value
+let domMuffinButton = document
+  .querySelector(".theButton")
+  .addEventListener("click", function () {
+    globalScore++;
+    domScoreLoader(globalScore);
+    audio.play();
+  });
+//button to add an upgrade
+document.querySelector(".perks1").addEventListener("click", function () {
+  upgradeScore = 1;
+  incrementValue();
+});
+document.querySelector(".perks2").addEventListener("click", function () {
+  upgradeScore = 0;
+  incrementValue();
+});
